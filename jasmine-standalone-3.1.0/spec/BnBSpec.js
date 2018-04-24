efb4e3cd3dd8454b21bea53f0a454bae90824808
marydomashneva <mary.domashneva@gmail.com>
2018-04-24 13:55:20 +0100

    Method delete by property id and test covered

--- jasmine-standalone-3.1.0/spec/BnBSpec.js
+++ jasmine-standalone-3.1.0/spec/BnBSpec.js
@@ -8,13 +8,16 @@ describe('BnB', function() {
   var note;
   var price;
   var newProp;
-  var expectedProperty;
+  var expectedPropertyFirst;
+  var expectedPropertySecond
   var propertyArray;
+  var propertyId;
 
   beforeEach(function() {
     bnb = new BnB();
     prop = new Property();
-    expectedProperty = bnb.createProperty('London', 'NiceFlat', 'Available', 50);
+    expectedPropertyFirst = bnb.createProperty(1, 'London', 'NiceFlat', 'Available', 50);
+    expectedPropertySecond = bnb.createProperty(2, 'London', 'BadFlat', 'Available', 30);
   });
 
   it('takes property as argument and adds it to array', function() {
@@ -23,17 +26,37 @@ describe('BnB', function() {
   });
 
   it('creates a new property', function() {
+    propertyId = 3
     city = 'London';
     name = 'NiceHouse';
     note = 'Available now!';
     price = 40;
-    newProp = bnb.createProperty(city, name, note, price);
+    newProp = bnb.createProperty(propertyId, city, name, note, price);
     bnb.addProperty(newProp);
     expect(bnb.properties).toEqual([newProp]);
   });
 
   it('lists all properties', function() {
-    bnb.addProperty(expectedProperty);
+    bnb.addProperty(expectedPropertyFirst);
     expect(bnb.listAllProperty()).toEqual(['NiceFlat']);
+  });
+
+  it('finds the correct property from the list', function() {
+    bnb.addProperty(expectedPropertyFirst);
+    bnb.addProperty(expectedPropertySecond);
+    expect(bnb.findProperty(2)).toEqual(expectedPropertySecond);
+  });
+
+  it('returns an id of property in array', function() {
+    bnb.addProperty(expectedPropertyFirst);
+    bnb.addProperty(expectedPropertySecond);
+    expect(bnb.findPropertyIndex(2)).toEqual(1);
+  });
+
+  it('deletes a property from the list', function() {
+    bnb.addProperty(expectedPropertyFirst);
+    bnb.addProperty(expectedPropertySecond);
+    bnb.deleteProperty(1);
+    expect(bnb.properties).toEqual([expectedPropertySecond]);
   });
 });
--- jasmine-standalone-3.1.0/src/BnB.js
+++ jasmine-standalone-3.1.0/src/BnB.js
@@ -8,8 +8,9 @@ BnB.prototype.addProperty = function(property) {
   this.properties.push(property);
 };
 
- BnB.prototype.createProperty = function(city, name, note, price) {
+ BnB.prototype.createProperty = function(propertyId, city, name, note, price) {
    var property = new Property();
+   property.id = propertyId;
    property.city = city;
    property.name = name;
    property.note = note;
@@ -27,10 +28,24 @@ BnB.prototype.addProperty = function(property) {
 
  };
 
- BnB.prototype.deleteProperty = function() {
-
+ BnB.prototype.findProperty  = function(propertyId) {
+   return this.properties.find(function(element) {
+     return element.id === propertyId;
+     return element.id
+   });
  };
 
+BnB.prototype.findPropertyIndex = function(propertyId) {
+  var index = this.properties.findIndex(function(property, index, properties) {
+    return property.id === propertyId
+   });
+   return index
+ };
+
+BnB.prototype.deleteProperty = function(propertyId) {
+  var index = this.findPropertyIndex(propertyId)
+  this.properties.splice(index, 1);
+ };
 
  BnB.prototype.bookProperty = function() {
 
